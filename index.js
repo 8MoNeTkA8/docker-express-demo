e12d688bb939bad4d71fb5189bd7144435bf76d8
8MoNeTkA8 <davronovigor555@gmail.com>
2026-04-02 22:28:02 +0300

    Test workflow

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Docker! This is version 2.2</h1>')
+  res.send('<h1>Hello from Docker! This is version after render update!</h1>')
 })
 
-const PORT = 8080
+const PORT = 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
